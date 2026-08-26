62b6b5a8970a93d453500b50ee1b8f0646c61d97
Akseli Arisalo <arisalo.akseli@hotmail.fi>
2026-08-27 02:14:19 +0300

    fix for btop not showing cpu wattage

--- system/btop/default.nix
+++ system/btop/default.nix
@@ -3,4 +3,9 @@
   environment.systemPackages = [
     pkgs.btop-rocm
   ];
+
+  # Fix for btop not showing cpu wattage
+  systemd.tmpfiles.rules = [
+    "Z /sys/class/powercap/intel-rapl:0/energy_uj 0444 root root - -"
+  ];
 }
